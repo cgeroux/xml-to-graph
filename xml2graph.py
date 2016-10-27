6a7d34ee266d594fcd7f02e2f387d0bfc0e3785a
cgeroux <chris.m.geroux@gmail.com>
2016-10-27 14:37:12 +0100

    Added stereotypes
    also tweaked a few other display issuse

--- xml2graph.py
+++ xml2graph.py
@@ -5,93 +5,231 @@ from lxml import etree
 import os
 import graphviz as gv
 
-class Method(object):
-  def __init__(self,xmlMethod):
+class Parameter(object):
+  """Holds data pertaining to a method parameter and generates a label for that
+  parameter.
+  """
+  
+  def __init__(self,xmlParamter):
+    """Save parameter attributes from xml node
+    """
     
     #get name
-    self.name=xmlMethod.find("name").text
-    print(self.name)
+    self.name=xmlParamter.find("name").text
     
-    #get visibility
-    xmlVisilibity=xmlMethod.find("visibility")
-    self.visibility=""
-    if xmlVisilibity!=None:
-      self.visibility=xmlVisilibity.text
+    #get type
+    xmlType=xmlParamter.find("type")
+    self.type=None
+    if xmlType!=None:
+      self.type=xmlType.text
+      
+    #get direction
+    xmlDirection=xmlParamter.find("direction")
+    self.direction=None
+    if xmlDirection!=None:
+      self.direction=xmlDirection.text
+  def getLabel(self):
+    """Returns a string to use in a dot diagram to describe this parameter.
+    """
     
+    label=""
+    
+    #add visibility if we have it
+    if self.direction!=None:
+      label+=self.direction+" "
+    
+    #add name
+    label+=self.name
+    
+    #add type if we have it
+    if self.type!=None:
+      label+=" : "+self.type
+    return label
 class Attribute(object):
+  """Holds data pertaining to a class attributes and generates a label for that
+  attribute.
+  """
+  
   def __init__(self,xmlAttribute):
+    """Save attribute attributes from xml node
+    """
     
     #get name
     self.name=xmlAttribute.find("name").text
-    print(self.name)
     
     #get visibility
     xmlVisilibity=xmlAttribute.find("visibility")
-    self.visibility=""
+    self.visibility=None
     if xmlVisilibity!=None:
       self.visibility=xmlVisilibity.text
       
     #get type
     xmlType=xmlAttribute.find("type")
-    self.type=""
+    self.type=None
     if xmlType!=None:
       self.type=xmlType.text
       
     #get value
     xmlValue=xmlAttribute.find("value")
-    self.value=""
+    self.value=None
     if xmlValue!=None:
       self.value=xmlValue.text
+  def getLabel(self):
+    """Returns a string to use in a dot diagram to describe this class 
+    attribute.
+    """
+    
+    label=""
+    
+    #add visibility if we have it
+    if self.visibility!=None:
+      label+=self.visibility+" "
+    
+    #add name
+    label+=self.name
+    
+    #add type if we have it
+    if self.type!=None:
+      label+=" : "+self.type
+    
+    #add value if we have it
+    if self.value!=None:
+      label+=" = "+self.value
+    return label
+class Method(object):
+  """
+  """
+  
+  def __init__(self,xmlMethod):
+    """
+    """
+    
+    #get name
+    self.name=xmlMethod.find("name").text
+    
+    #get visibility
+    xmlVisilibity=xmlMethod.find("visibility")
+    self.visibility=None
+    if xmlVisilibity!=None:
+      self.visibility=xmlVisilibity.text
+    
+    #get return type
+    xmlReturnType=xmlMethod.find("return-type")
+    self.returnType=None
+    if xmlReturnType!=None:
+      self.returnType=xmlReturnType.text
+    
+    #get parameters
+    self.parameters=[]
+    xmlParameters=xmlMethod.find("parameters")
+    for xmlParameter in xmlParameters:
+      self.parameters.append(Parameter(xmlParameter))
+  def getLabel(self):
+    """
+    """
+    
+    label=""
+    
+    #add visibility if we have it
+    if self.visibility!=None:
+      label+=self.visibility+" "
+    
+    #add name
+    label+=self.name
+    
+    #add parameters if we have then
+    if len(self.parameters)>0:
+      label+="("
+      count=0
+      for parameter in self.parameters:
+        if count==0:
+          label+=parameter.getLabel()
+          count+=1
+        else:
+          label+=", "+parameter.getLabel()
+          count+=1
+      label+=")"
+    label+="\l"
+    return label
 class Class(object):
+  """
+  """
+  
   def __init__(self,xmlClass):
+    """
+    """
     
     #get name
     self.name=xmlClass.find("name").text
-    print(self.name)
+    
+    #get stereotype
+    xmlStereotype=xmlClass.find("stereotype")
+    self.stereotype=None
+    if xmlStereotype!=None:
+      self.stereotype=xmlStereotype.text
     
     #get attributes
     xmlAttributes=xmlClass.find("attributes")
     self.attributes=[]
-    for xmlAttribute in xmlAttributes:
-      attributeTemp=Attribute(xmlAttribute)
-      self.attributes.append(attributeTemp)
+    if xmlAttributes!=None:
+      for xmlAttribute in xmlAttributes:
+        attributeTemp=Attribute(xmlAttribute)
+        self.attributes.append(attributeTemp)
     
     #get methods
     xmlMethods=xmlClass.find("methods")
     self.methods=[]
-    for xmlMethod in xmlMethods:
-      methodTemp=Method(xmlMethod)
-      self.methods.append(methodTemp)
-  def getNodeLabel(self):
+    if xmlMethods!=None:
+      for xmlMethod in xmlMethods:
+        methodTemp=Method(xmlMethod)
+        self.methods.append(methodTemp)
+  def getLabel(self):
+    """
+    """
     
+    label="{"
+    
+    #add stereotype if we have one
+    if self.stereotype!=None:
+      #label+=self.stereotype+"\l"
+      label+="\<\<"+self.stereotype+"\>\> \\n"
+      
     #add class name
-    label="{"+self.name+"|"
+    label+=self.name
+    
+    if len(self.attributes)>0 or len(self.methods)>0:
+      label+="|"
     
     #add attributes
     for attribute in self.attributes:
+      label+=attribute.getLabel()+"\l"
     
-      label+=attribute.visibility+" "+attribute.name
-      if attribute.type!="":
-        label+=" : "+attribute.type
-      if attribute.value!="":
-        label+=" = "+attribute.value
-      label+="\l"
-    
-    label+="|"
+    #add attribute method seperator if we have methods
+    if len(self.methods)>0:
+      label+="|"
     
     #add methods
     for method in self.methods:
-      label+=method.visibility+" "+method.name+"\l"
+      label+=method.getLabel()+"\l"
+    
     label+="}"
     return label
   def addToGraph(self,graph):
-    graph.node(name=self.name,label=self.getNodeLabel(),shape="record")
+    """
+    """
+    
+    graph.node(name=self.name,label=self.getLabel(),shape="record"
+    ,fontname="courier new")
 class Package(object):
+  """
+  """
+  
   def __init__(self,xmlPackage):
+    """
+    """
     
     #get name
     self.name=xmlPackage.find("name").text
-    print(self.name)
     
     #get classes
     xmlClasses=xmlPackage.find("classes")
@@ -100,6 +238,9 @@ class Package(object):
       classTemp=Class(xmlClass)
       self.classes.append(classTemp)
   def getGraph(self):
+    """
+    """
+    
     graph=gv.Digraph(self.name)
     
     #add classes to graph
@@ -107,6 +248,9 @@ class Package(object):
       classTemp.addToGraph(graph)
     return graph
 class PackageManager(object):
+  """
+  """
+  
   def __init__(self,xmlPackages):
     """
     """
@@ -117,26 +261,36 @@ class PackageManager(object):
       #create a packages
       package=Package(xmlPackage)
       self.packages.append(package)
-  def getGraph(self):
-    graph=gv.Digraph(format="pdf")
+  def getGraph(self,format="pdf"):
+    """
+    """
+    
+    graph=gv.Digraph(format=format)
+    graph
     for package in self.packages:
       graph.subgraph(package.getGraph())
     return graph
-  def drawGraph(self):
+  def drawGraph(self,fileName):
+    """
+    """
     
     #get the graph to draw
     graph=self.getGraph()
     
-    #save as "test" dot file and render to pdf as "test.pdf"
-    filename=graph.render(filename="test")
+    #save as fileName dot file and render to pdf as fileName+".pdf"
+    filename=graph.render(filename=fileName)
 def parseOptions():
   """Parses command line options
-  
   """
   
   parser=op.OptionParser(usage="Usage: %prog [options] XMLINPUT"
     ,version="%prog 1.0"
     ,description="Draws a graph from the given XMLINPUT file")
+  
+  parser.add_option("-o",action="store"
+    ,dest="outputFileName"
+    ,help="Sets the output file name. File extension added automatically "
+    +"[default: %default].",default="graph")
   
   #parse command line options
   return parser.parse_args()
@@ -168,7 +322,8 @@ def main():
   #Parse XML Packages
   xmlPackages=tree.getroot()
   packageManager=PackageManager(xmlPackages)
-  packageManager.drawGraph()
   
+  #draw graph
+  packageManager.drawGraph(options.outputFileName)
 if __name__ == "__main__":
  main()
